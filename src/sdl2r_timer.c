ffbcac91fe03c2bf6ae954ff5317ee1dad49a180
mirichi <sawara01@gmail.com>
2014-10-25 09:38:18 +0900

    タイマのメソッド追加

--- src/sdl2r_timer.c
+++ src/sdl2r_timer.c
@@ -9,17 +9,35 @@ VALUE sdl2r_delay(VALUE klass, VALUE vms)
     return Qnil;
 }
 
+
 VALUE sdl2r_get_ticks(VALUE klass)
 {
     Uint32 ticks = SDL_GetTicks();
     return UINT2NUM(ticks);
 }
 
+
+VALUE sdl2r_get_performance_counter(VALUE klass)
+{
+    Uint64 count = SDL_GetPerformanceCounter();
+    return LL2NUM(count);
+}
+
+
+VALUE sdl2r_get_performance_frequency(VALUE klass)
+{
+    Uint64 freq = SDL_GetPerformanceFrequency();
+    return LL2NUM(freq);
+}
+
+
 void Init_sdl2r_timer(void)
 {
     // SDL module methods
     rb_define_singleton_method(mSDL, "delay", sdl2r_delay, 1);
     rb_define_singleton_method(mSDL, "get_ticks", sdl2r_get_ticks, 0);
+    rb_define_singleton_method(mSDL, "get_performance_counter", sdl2r_get_performance_counter, 0);
+    rb_define_singleton_method(mSDL, "get_performance_frequency", sdl2r_get_performance_frequency, 0);
 }
 
 
