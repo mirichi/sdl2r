5893523a7e0cd85f5463c51d761b996d4497e7d6
mirichi <sawara01@gmail.com>
2014-11-01 07:44:23 +0900

    マクロを利用するようにclipboardを変更

--- src/sdl2r_clipboard.c
+++ src/sdl2r_clipboard.c
@@ -9,9 +9,7 @@ VALUE sdl2r_get_clipboard_text(VALUE klass)
     VALUE vresult;
 
     result = SDL_GetClipboardText();
-    if (!result) {
-        rb_raise(eSDLError, SDL_GetError());
-    }
+    SDL2R_CHECK_RESULT(result);
 
     vresult = SDL2R_TO_UTF8_STRING(result);
     SDL_free(result);
@@ -22,16 +20,15 @@ VALUE sdl2r_get_clipboard_text(VALUE klass)
 
 VALUE sdl2r_has_clipboard_text(VALUE klass)
 {
-    return SDL_HasClipboardText() ? Qtrue : Qfalse;
+    SDL_bool result = SDL_HasClipboardText();
+    return SDL2R_TO_BOOL(result);
 }
 
 
 VALUE sdl2r_set_clipboard_text(VALUE klass, VALUE vstr)
 {
-    if (SDL_SetClipboardText(SDL2R_TO_UTF8_PTR(vstr))) {
-        rb_raise(eSDLError, SDL_GetError());
-    }
-
+    int result = SDL_SetClipboardText(SDL2R_TO_UTF8_PTR(vstr));
+    SDL2R_CHECK_RESULT(result == 0);
     return vstr;
 }
 
@@ -39,9 +36,9 @@ VALUE sdl2r_set_clipboard_text(VALUE klass, VALUE vstr)
 void Init_sdl2r_clipboard(void)
 {
     // SDL module methods
-    rb_define_singleton_method(mSDL, "get_clipboard_text", sdl2r_get_clipboard_text, 0);
-    rb_define_singleton_method(mSDL, "has_clipboard_text", sdl2r_has_clipboard_text, 0);
-    rb_define_singleton_method(mSDL, "set_clipboard_text", sdl2r_set_clipboard_text, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, get_clipboard_text, 0);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, has_clipboard_text, 0);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, set_clipboard_text, 1);
 }
 
 
