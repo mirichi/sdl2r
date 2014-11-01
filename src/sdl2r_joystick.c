3918373fb876f10b89ebb2e096a4ee851872b596
mirichi <sawara01@gmail.com>
2014-11-01 17:02:14 +0900

    joystick_name、joystick_instance_id追加

--- src/sdl2r_joystick.c
+++ src/sdl2r_joystick.c
@@ -157,6 +157,34 @@ static VALUE sdl2r_joystick_event_state(VALUE klass, VALUE vstate)
 }
 
 
+static VALUE sdl2r_joystick_name(VALUE klass, VALUE vjoystick)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    const char* result;
+
+    result = SDL_JoystickName(js->joystick);
+    if (!result) {
+        return Qnil;
+    }
+
+    return SDL2R_TO_UTF8_STRING(result);
+}
+
+
+static VALUE sdl2r_joystick_instance_id(VALUE klass, VALUE vjoystick)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    SDL_JoystickID result;
+
+    result = SDL_JoystickInstanceID(js->joystick);
+    if (result < 0) {
+        rb_raise(eSDLError, SDL_GetError());
+    }
+
+    return INT2NUM(result);
+}
+
+
 void Init_sdl2r_joystick(void)
 {
     // SDL module methods
@@ -168,6 +196,20 @@ void Init_sdl2r_joystick(void)
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_num_buttons, 1);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_num_hats, 1);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_event_state, 1);
+
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_attached, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_axis, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_ball, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_button, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_device_guid, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_from_string, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_string, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_hat, 0);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_instance_id, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_name, 1);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_name_for_index, 0);
+//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_update, 0);
 
     // SDL::Joystick class
     cJoystick = rb_define_class_under(mSDL, "Joystick", rb_cObject);
