19b92add27d71865cf4a4cf302e3f78b0b5c459e
mirichi <sawara01@gmail.com>
2014-11-02 14:24:11 +0900

    Joystick完成

--- src/sdl2r_joystick.c
+++ src/sdl2r_joystick.c
@@ -192,6 +192,130 @@ static VALUE sdl2r_joystick_instance_id(VALUE klass, VALUE vjoystick)
 }
 
 
+static VALUE sdl2r_joystick_get_attached(VALUE klass, VALUE vjoystick)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_STRUCT(Joystick, vjoystick);
+    SDL_bool result;
+
+    result = SDL_JoystickGetAttached(js->joystick);
+
+    return SDL2R_TO_BOOL(result);
+}
+
+
+static VALUE sdl2r_joystick_get_axis(VALUE klass, VALUE vjoystick, VALUE vaxis)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    Sint16 result;
+
+    result = SDL_JoystickGetAxis(js->joystick, NUM2INT(vaxis));
+
+    return INT2NUM(result);
+}
+
+
+static VALUE sdl2r_joystick_get_ball(VALUE klass, VALUE vjoystick, VALUE vball)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    int result, dx, dy;
+    
+
+    result = SDL_JoystickGetBall(js->joystick, NUM2INT(vball), &dx, &dy);
+    if (result < 0) {
+        rb_raise(eSDLError, SDL_GetError());
+    }
+
+    return rb_ary_new3(2, INT2NUM(dx), INT2NUM(dy));
+}
+
+
+static VALUE sdl2r_joystick_get_button(VALUE klass, VALUE vjoystick, VALUE vbutton)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    int result;
+
+    result = SDL_JoystickGetButton(js->joystick, NUM2INT(vbutton));
+
+    return UINT2NUM(result);
+}
+
+
+static VALUE sdl2r_joystick_get_device_guid(VALUE klass, VALUE vdevice_index)
+{
+    SDL_JoystickGUID js_guid = SDL_JoystickGetDeviceGUID(NUM2INT(vdevice_index));
+    return rb_str_new((char *)&js_guid, sizeof(SDL_JoystickGUID));
+}
+
+
+static VALUE sdl2r_joystick_get_guid_string(VALUE klass, VALUE vguid)
+{
+    SDL_JoystickGUID js_guid;
+    char buf[1024];
+
+    Check_Type(vguid, T_STRING);
+    if (RSTRING_LEN(vguid) != sizeof(js_guid.data)) {
+        rb_raise(eSDL2RError, "not guid");
+    }
+    SDL_memcpy(js_guid.data, RSTRING_PTR(vguid), sizeof(js_guid.data));
+    SDL_JoystickGetGUIDString(js_guid, buf, 1024);
+
+    return SDL2R_TO_UTF8_STRING(buf);
+}
+
+
+static VALUE sdl2r_joystick_get_guid(VALUE klass, VALUE vjoystick)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_STRUCT(Joystick, vjoystick);
+    SDL_JoystickGUID js_guid;
+
+    js_guid = SDL_JoystickGetGUID(js->joystick);
+
+    return rb_str_new((char *)&js_guid, sizeof(SDL_JoystickGUID));
+}
+
+
+static VALUE sdl2r_joystick_get_guid_from_string(VALUE klass, VALUE vstr)
+{
+    SDL_JoystickGUID js_guid;
+
+    Check_Type(vstr, T_STRING);
+    js_guid = SDL_JoystickGetGUIDFromString(RSTRING_PTR(vstr));
+
+    return rb_str_new((char *)&js_guid, sizeof(SDL_JoystickGUID));
+}
+
+
+static VALUE sdl2r_joystick_get_hat(VALUE klass, VALUE vjoystick, VALUE vhat)
+{
+    struct SDL2RJoystick *js = SDL2R_GET_JOYSTICK_STRUCT(vjoystick);
+    Uint8 result;
+
+    result = SDL_JoystickGetHat(js->joystick, NUM2INT(vhat));
+
+    return UINT2NUM(result);
+}
+
+
+static VALUE sdl2r_joystick_name_for_index(VALUE klass, VALUE vdevice_index)
+{
+    const char* result;
+
+    result = SDL_JoystickNameForIndex(NUM2INT(vdevice_index));
+    if (!result) {
+        return Qnil;
+    }
+
+    return SDL2R_TO_UTF8_STRING(result);
+}
+
+
+static VALUE sdl2r_joystick_update(VALUE klass)
+{
+    SDL_JoystickUpdate();
+    return Qnil;
+}
+
+
 void Init_sdl2r_joystick(void)
 {
     // SDL module methods
@@ -203,20 +327,19 @@ void Init_sdl2r_joystick(void)
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_num_buttons, 1);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_num_hats, 1);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_event_state, 1);
-
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_attached, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_axis, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_ball, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_button, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_device_guid, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_from_string, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_string, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_hat, 0);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_attached, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_axis, 2);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_ball, 2);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_button, 2);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_device_guid, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_from_string, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_guid_string, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_get_hat, 2);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_instance_id, 1);
     SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_name, 1);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_name_for_index, 0);
-//    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_update, 0);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_name_for_index, 1);
+    SDL2R_DEFINE_SINGLETON_METHOD(mSDL, joystick_update, 0);
 
     // SDL::Joystick class
     cJoystick = rb_define_class_under(mSDL, "Joystick", rb_cObject);
@@ -224,5 +347,5 @@ void Init_sdl2r_joystick(void)
     rb_define_method(cJoystick, "dispose", sdl2r_joystick_im_dispose, 0);
     rb_define_method(cJoystick, "disposed?", sdl2r_joystick_im_get_disposed, 0);
 
-    sdl2r_joystick_hash = sdl2r_hash_alloc(8);
+    sdl2r_joystick_hash = sdl2r_hash_alloc(4);
 }
