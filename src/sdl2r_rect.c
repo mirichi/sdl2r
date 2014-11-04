66eb046f8258a80ed0dce20e7b7fa602565bfe11
mirichi <sawara01@gmail.com>
2014-11-04 21:31:43 +0900

    enclose_points追加

--- src/sdl2r_rect.c
+++ src/sdl2r_rect.c
@@ -5,6 +5,41 @@
 VALUE cRect;
 VALUE cPoint;
 
+VALUE sdl2r_enclose_points(VALUE klass, VALUE vpoints, VALUE vclip)
+{
+    SDL_Rect clip, result;
+    SDL_Rect *pclip=0;
+    int i;
+    SDL_bool bool;
+    VALUE ary[4];
+
+    if (vclip != Qnil) {
+        SDL2R_SET_RECT(clip, vclip);
+        pclip = &clip;
+    }
+
+    Check_Type(vpoints, T_ARRAY);
+    {
+        SDL_Point points[RARRAY_LEN(vpoints)];
+        for (i = 0; i < RARRAY_LEN(vpoints); i++) {
+            Check_Type(rb_ary_entry(vpoints, i), T_ARRAY);
+            SDL2R_SET_POINT(points[i], rb_ary_entry(vpoints, i));
+        }
+        bool = SDL_EnclosePoints(points, RARRAY_LEN(vpoints), pclip, &result);
+    }
+
+    if (bool == SDL_FALSE) {
+        return Qnil;
+    }
+
+    ary[0] = INT2NUM(result.x);
+    ary[1] = INT2NUM(result.y);
+    ary[2] = INT2NUM(result.w);
+    ary[3] = INT2NUM(result.h);
+    return rb_class_new_instance(4, ary, cRect);
+}
+
+
 VALUE sdl2r_has_intersection(VALUE klass, VALUE vrecta, VALUE vrectb)
 {
     SDL_Rect a, b;
@@ -27,7 +62,7 @@ void Init_sdl2r_rect(void)
     cRect = rb_define_class_under(mSDL, "Rect", rb_cArray);
 
     // SDL module methods
-//    SDL2R_DEFINE_SINGLETON_METHOD(enclose_points, 2);
+    SDL2R_DEFINE_SINGLETON_METHOD(enclose_points, 2);
     SDL2R_DEFINE_SINGLETON_METHOD(has_intersection, 2);
 //    SDL2R_DEFINE_SINGLETON_METHOD(intersect_rect, 2);
 //    SDL2R_DEFINE_SINGLETON_METHOD(intersect_rect_and_line, 3);
